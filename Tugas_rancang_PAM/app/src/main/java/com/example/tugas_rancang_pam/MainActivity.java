bbde21e33713241d459906343b8a121a9bfd94e2
Resa Renaldy <resarenaldy77@gmail.com>
2020-03-31 18:41:19 +0700

    update awal aplikasi
    buat tampilan welcome sama homwnya

--- Tugas_rancang_PAM/app/src/main/java/com/example/tugas_rancang_pam/MainActivity.java
+++ Tugas_rancang_PAM/app/src/main/java/com/example/tugas_rancang_pam/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tugas_rancang_pam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, Home.class) ;
+                startActivity(homeIntent);
+                finish();
+            }
+        },WELCOME_TIME_OUT);
+
+
     }
 }

d4722424e3468462788f478335ace2fc42033cb5
Resa Renaldy <resarenaldy77@gmail.com>
2020-04-09 17:04:31 +0700

    bersihkan data, update contact us permission

--- Tugas_rancang_PAM/app/src/main/java/com/example/tugas_rancang_pam/ContactusActivity.java
+++ Tugas_rancang_PAM/app/src/main/java/com/example/tugas_rancang_pam/ContactusActivity.java
@@ -1,7 +1,10 @@
 package com.example.tugas_rancang_pam;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,27 +12,67 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class ContactusActivity extends AppCompatActivity {
     Button mDialbtn;
+    private final int SHOW_CALL_PHONE = 1;
+    private final int CALL_PHONE_PERMISSION = 2;
+    private String[] PERMISSIONS = {Manifest.permission.CALL_PHONE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactus);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)) {
+            for (String permission : PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(ContactusActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ContactusActivity.this, PERMISSIONS, CALL_PHONE_PERMISSION);
+                }
+            }
+        }
+
+
         mDialbtn=findViewById(R.id.dialbtn);
-        String number = "6281231925666";
         mDialbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String number = "6281231925666";
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+Uri.encode(number)));
-                startActivity(intent);
+
+                if (checkPermission()){
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:085720037803"));
+                    startActivity(callIntent);
+                }else{
+                    if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)) {
+                        for (String permission : PERMISSIONS) {
+                            if (ActivityCompat.checkSelfPermission(ContactusActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+                                ActivityCompat.requestPermissions(ContactusActivity.this, PERMISSIONS, CALL_PHONE_PERMISSION);
+                            }
+                        }
+                    }
+                }
             }
         });
 
     }
+
+    public boolean checkPermission(){
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)) {
+            for (String permission : PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(ContactusActivity.this, permission) == PackageManager.PERMISSION_GRANTED) {
+                    return true ;
+                }
+                else{
+                    return false ;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
